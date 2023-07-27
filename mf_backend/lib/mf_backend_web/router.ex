a66063bc06bb403014fcf71b26333bacca27ba16
George Savchenko <george.savchenko@gmail.com>
2023-07-27 13:39:38 -0400

    backend deps - add cors for cross port comm

--- mf_backend/lib/mf_backend_web/router.ex
+++ mf_backend/lib/mf_backend_web/router.ex
@@ -2,28 +2,30 @@ defmodule MfBackendWeb.Router do
   use MfBackendWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, html: {MfBackendWeb.Layouts, :root}
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_live_flash)
+    plug(:put_root_layout, html: {MfBackendWeb.Layouts, :root})
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
+    # TODO: this is a security risk, update this to only allow the frontend
+    plug(CORSPlug, origin: "*")
   end
 
   scope "/", MfBackendWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", PageController, :home
+    get("/", PageController, :home)
   end
 
   scope "/api", MfBackendWeb do
-    pipe_through :api
+    pipe_through(:api)
 
-    resources "/lobbies", LobbyController, only: [:index, :create, :edit]
+    resources("/lobbies", LobbyController, only: [:index, :create, :edit])
   end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
@@ -36,10 +38,10 @@ defmodule MfBackendWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
-      pipe_through :browser
+      pipe_through(:browser)
 
-      live_dashboard "/dashboard", metrics: MfBackendWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+      live_dashboard("/dashboard", metrics: MfBackendWeb.Telemetry)
+      forward("/mailbox", Plug.Swoosh.MailboxPreview)
     end
   end
 end

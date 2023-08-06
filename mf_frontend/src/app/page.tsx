ef39711e00dfd33e333827ef40c9ed126969e0b6
Ben Xia <bzijiexia@gmail.com>
2023-08-06 10:23:28 -0700

    added keyboard controls

--- mf_frontend/src/app/page.tsx
+++ mf_frontend/src/app/page.tsx
@@ -1,25 +1,58 @@
 "use client";
 
 import { usePhoenixSocket } from "@/features";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import styles from "./page.module.css";
-import { Floor } from "./components";
+import {
+  KeyboardControls,
+  KeyboardControlsEntry,
+  OrbitControls,
+} from "@react-three/drei";
+import Floor from "./components/Floor";
+import { Grid } from "./components/three/grid";
+import { Player } from "./components/three/player";
+import { useMemo } from "react";
+
+enum Controls {
+  forward = "forward",
+  back = "back",
+  left = "left",
+  right = "right",
+  jump = "jump",
+}
 
 export default function Home() {
+  const map = useMemo<KeyboardControlsEntry<Controls>[]>(
+    () => [
+      { name: Controls.forward, keys: ["ArrowUp", "KeyW"] },
+      { name: Controls.back, keys: ["ArrowDown", "KeyS"] },
+      { name: Controls.left, keys: ["ArrowLeft", "KeyA"] },
+      { name: Controls.right, keys: ["ArrowRight", "KeyD"] },
+    ],
+    [],
+  );
+
   usePhoenixSocket();
 
   return (
     <div className={styles.scene}>
-      <Canvas
-        shadows
-        className={styles.canvas}
-        camera={{
-          position: [-6, 7, 7],
-        }}
-      >
-        <ambientLight color={"white"} intensity={0.3} />
-        <Floor position={[0, -1, 0]} />
-      </Canvas>
+      <KeyboardControls map={map}>
+        <Canvas
+          shadows
+          className={styles.canvas}
+          camera={{
+            position: [0, 0, 50],
+            // near: 1,
+          }}
+        >
+          <ambientLight color={"white"} intensity={0.3} />
+          {/* <Floor position={[0, -1, 0]} /> */}
+          <Grid />
+          <OrbitControls makeDefault />
+          <axesHelper args={[5]} />
+          <Player />
+        </Canvas>
+      </KeyboardControls>
     </div>
   );
 }

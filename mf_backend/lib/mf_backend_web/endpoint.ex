db0ed78ca108dd633484d16d0c597cada15bd41d
George Savchenko <george.savchenko@gmail.com>
2023-10-26 14:06:17 -0400

    adding user socket to endpoint

--- mf_backend/lib/mf_backend_web/endpoint.ex
+++ mf_backend/lib/mf_backend_web/endpoint.ex
@@ -12,6 +12,8 @@ defmodule MfBackendWeb.Endpoint do
   ]
 
   socket("/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]])
+  socket("/socket", MfBackendWeb.UserSocket)
+
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest

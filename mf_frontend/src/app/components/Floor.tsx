a9a5a0405ecb95002e13ae6ca17215569f8bb6ba
George Savchenko <george.savchenko@gmail.com>
2023-11-04 13:58:21 -0400

    initial three setup

---
+++ mf_frontend/src/app/components/Floor.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { ThreeElements, extend } from "@react-three/fiber";
+import { Mesh, BoxGeometry, MeshPhysicalMaterial, Group } from "three";
+import React from "react";
+
+export function Floor(props: ThreeElements["mesh"]) {
+  return (
+    <mesh {...props} receiveShadow>
+      <boxGeometry args={[20, 1, 10]} />
+      <meshPhysicalMaterial color="white" />
+    </mesh>
+  );
+}
+
+extend({ BoxGeometry });
+
+export default Floor;

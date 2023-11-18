4bfdce20d3e13e83bd7d0a652dfa8af8426c3066
George Savchenko <george.savchenko@gmail.com>
2023-11-17 20:15:49 -0500

    make grid component more declarative

--- mf_frontend/src/app/components/three/grid.component.tsx
+++ mf_frontend/src/app/components/three/grid.component.tsx
@@ -1,9 +1,26 @@
-import { useFrame } from "@react-three/fiber";
+import { Vector3 } from "three";
 
 export const Grid = () => {
+  // Define the properties for planeGeometry
+  const geometryProps = {
+    width: 100,
+    height: 50,
+    widthSegments: 5,
+    heightSegments: 5,
+  };
+
+  const meshPosition = new Vector3(0, 0, 0);
+
   return (
-    <mesh position={[0, 0, 0]}>
-      <planeGeometry args={[100, 50, 5, 5]} />
+    <mesh position={meshPosition}>
+      <planeGeometry
+        args={[
+          geometryProps.width,
+          geometryProps.height,
+          geometryProps.widthSegments,
+          geometryProps.heightSegments,
+        ]}
+      />
       <meshBasicMaterial color="green" />
     </mesh>
   );

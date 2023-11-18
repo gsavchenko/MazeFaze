1eceb677e540343c131442696e49191045002007
George Savchenko <george.savchenko@gmail.com>
2023-11-17 21:01:35 -0500

    make game larger

--- mf_frontend/src/app/page.tsx
+++ mf_frontend/src/app/page.tsx
@@ -5,12 +5,21 @@ import { Canvas } from "@react-three/fiber";
 import styles from "./page.module.css";
 import { Grid } from "./components/three/grid.component";
 import { Player } from "@/features/player";
+import styled from "styled-components";
+
+const StyledScene = styled.div`
+  width: 90vw; // 100% of the viewport width
+  height: 90vh; // 100% of the viewport height
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default function Home() {
   usePhoenixSocket();
 
   return (
-    <div className={styles.scene}>
+    <StyledScene>
       <Canvas
         shadows
         className={styles.canvas}
@@ -24,6 +33,6 @@ export default function Home() {
         <axesHelper args={[5]} />
         <Player />
       </Canvas>
-    </div>
+    </StyledScene>
   );
 }
